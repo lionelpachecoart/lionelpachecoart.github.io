a7c9f5a573b2cfdbe5282cc096fd6f6be969b2da
lionelpachecoart <140350643+lionelpachecoart@users.noreply.github.com>
2023-08-03 11:36:29 -0300

    add php and js folder

---
+++ js/prueba.php.mailer.js
@@ -0,0 +1,42 @@
+ "use strict"
+ const forms = document.forms;
+ if(forms.length){
+  for(const form of forms){
+  const file = form.querySelector('input[type="file"]');
+  file ? file.addEventListener('change', formAddFile) : null;
+  form.addEventListener('submit', formSubmitAction);
+ }
+}
+
+function formAddFile(e){
+  const formInputFile = e.target;
+  const formFiles = formInputFile.files;
+  const fileName = formFiles.length ? formFiles[0].name : '';
+  formInputFile.parentElement.nextElementSibling.innerHTML = fileName;
+}
+
+async function formSubmitAction(e){
+  e.preventDefault();
+  const form = e.target;
+  const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : "#";
+  const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : "GET";
+  const formData = new FormData(form);
+
+  form.classList.add('form-sending');
+
+  const response = await fetch(formAction, {
+    method: formMethod,
+    body: formData,
+  })
+  if (response.ok){
+    alert('Formulario enviado exitosamente');
+    form.classList.remove('form-sending');
+
+    const formInputFile = form.querySelector('input[type="file"]');
+    formInputFile ? formInputFile.parentElement.nextElementSibling.innerHTML = fileName = '' : null;
+    form.reset();
+  }else{
+    alert('No se pudo enviar el formulario');
+    form.classList.remove('form-sending');
+  }
+}
